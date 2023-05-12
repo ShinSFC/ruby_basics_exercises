0a96b3ec8559a9825799262dacf14ff01c83ec6e
ShinpeiH <dctechsf@gmail.com>
2023-05-12 01:19:30 -0700

    middle of user input exercises

---
+++ user_input/repeat_after_me.rb
@@ -0,0 +1,3 @@
+puts "Type something:"
+something = gets.chomp # chomp is not needed here for some reason
+puts "#{something}"
---
+++ user_input/your_age_in_months.rb
@@ -0,0 +1,4 @@
+puts "What is your age in years?"
+years = gets # does not need chomp
+months = years.to_i * 12
+puts "You are #{months} months old."
